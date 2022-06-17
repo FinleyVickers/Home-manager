b7aeb8ebe76d7c31a6a7ef664ed35dc04c98fc8d
Bango88 <47485594+Bango88@users.noreply.github.com>
2022-06-17 14:54:14 -0700

    Change version

--- home.nix
+++ home.nix
@@ -33,7 +33,7 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "20.09";
+  home.stateVersion = "22.05";
 
   # Since we do not install home-manager, you need to let home-manager
   # manage your shell, otherwise it will not be able to add its hooks
