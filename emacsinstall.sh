2312cd16116820f691d99d8e5ad12de75b85ea77
Finley Vickers <fvickers05@gmail.com>
2022-06-18 10:29:04 -0700

    General changes

---
+++ emacsinstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+
+~/.emacs.d/bin/doom install
--- home.nix
+++ home.nix
@@ -15,15 +15,57 @@
   #
   # You need to change these to match your username and home directory
   # path:
-  home.username = "$USER";
-  home.homeDirectory = "$HOME";
+  home.username = "finleyv";
+  home.homeDirectory = "/home/finleyv";
 
-  # If you use non-standard XDG locations, set these options to the
-  # appropriate paths:
-  #
-  # xdg.cacheHome
-  # xdg.configHome
-  # xdg.dataHome
+  nixpkgs.config.allowUnfree = true;  
+
+  home.packages = with pkgs; [
+    # Development/text editors
+    vscode
+    neovim
+    emacs
+
+    python3
+    nodejs
+    cmake
+    rust-analyzer
+    rustup
+
+    # Utils/cli misc.
+    pfetch
+    btop
+    rsync
+    rclone
+    git
+    gh
+    unzip
+    exa
+
+    # Gaming
+    steam
+    steam-run
+    polymc
+    discord
+    betterdiscordctl
+
+    # School
+    teams
+    obsidian
+    notion
+
+    # Gnome theming and settings
+    gnome3.gnome-tweaks
+    gnome-themes-extra
+    catppuccin-gtk
+
+    # Misc
+    blender
+    davinci-resolve
+  ];
+  
+  nixpkgs.overlays = [(self: super: { discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball https://discord.com/api/download/stable?platform=linux&format=tar.gz; });})];
+
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -35,16 +77,37 @@
   # changes in each release.
   home.stateVersion = "22.05";
 
-  # Since we do not install home-manager, you need to let home-manager
-  # manage your shell, otherwise it will not be able to add its hooks
-  # to your profile.
-  programs.bash = {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Start emacs server
+  services.emacs.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "cd ~/.config/home-manager && bash switch.sh";
+    };
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "robbyrussell";
+    };
+  };
+
+  programs.fish = {
     enable = true;
   };
 
-  home.packages = [
-    pkgs.htop
-    pkgs.fortune
-  ];
+  home = {
+    sessionPath = ["/home/finleyv/.emacs.d/bin"];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Finley Vickers";
+    userEmail = "fvickers05@gmail.com";
+  };
 
 }
